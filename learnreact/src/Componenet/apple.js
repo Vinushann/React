3fe56c9f4b348ea9bfcbffcd9d316344a032deaf
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-20 14:49:14 +0530

    about create teh CLASS component

--- learnreact/src/Componenet/apple.js
+++ learnreact/src/Componenet/apple.js
@@ -1,8 +1,24 @@
-// create new component
-function Apple(){
-  return(
-    <h2> APPLE component </h2>
-  );
+import Mango from './Mango';
+
+// directly get the Component class from react.
+import {Component} from 'react';
+
+// create class component
+// extends component class
+class Apple extends Component {
+
+  // cretae render method to send the data
+  render(){
+
+    // return statement
+    return(
+      <>
+       <h1>from apple class</h1>
+        <Mango/>
+      </>
+     
+    )
+  }
 }
 
 
