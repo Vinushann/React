9f99e57a14c00b86b43e4874c71cfa265882c989
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-24 00:50:17 +0530

    A12 - State with Object - for funtion

--- learnreact/src/Componenet/Color.js
+++ learnreact/src/Componenet/Color.js
@@ -3,29 +3,29 @@ import {useState} from 'react'
 import CarClass from './CarClass'
 function Color() {
   
-  // create multiple states
-  const [color, setColor] = useState('Red');
-  const [country, setCountry] = useState('Canada');
-  const [route, setRoute] = useState('67, hill road.');
+  // create multiple things via OBJECT
+  // obj = is contain all the data of that created object
+  // setObject = is for change the values and get the previous values.
+const [obj, setObject] = useState({
+  color : 'Red',
+  country : 'Canada',
+  route : 'No45, Canada'
+})
 
-  // set the color to Green
-  const increment = () => {
-    setColor('Green')
-  };
 
-  // set the color to Blue
-  const Blue = () => {
-    setColor('Blue')
-  };
-
+// change the data for that OBJECT
+function change(){
+  setObject(prevousState => {
+    return {...prevousState, color : 'Pink', country: 'Germany'}
+  })
+}
   return (
     <>
       <h1> From Color Component </h1>
-      <p>color is : {color}</p>
-      <p>country is : {country}</p>
-      <p>route is : {route}</p>
-      
-      
+      <p>color is : {obj.color}</p>
+      <p>country is : {obj.country}</p>
+      <p>route is : {obj.route}</p>
+      <button onClick={change} >change the color</button>
       <CarClass/>
     </>
   );
