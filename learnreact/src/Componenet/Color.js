e3d2e30fb01a3498dd6338a105d782b8c8107ad7
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-25 17:01:12 +0530

    A4 - useEffect

--- learnreact/src/Componenet/Color.js
+++ learnreact/src/Componenet/Color.js
@@ -1,39 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function List() {
-  const [list, setList] = useState([]);
-  // this state for update the number
-  const [num, setNum] = useState(1);
+  
+  const [count, setCount] = useState(1);
+  
+  useEffect(() => {
+    console.log('screen rendered');
+  }, [count]);
 
-  // function for change the value
-  function addItem() {
 
-    // dynamically change the value whenever user clicks button
-    const itemName = "Item" + num;
-
-    // set the list whenever user clicks button
-    setList((previousState) => {
-
-      // return old values with new values
-      return [...previousState, itemName];
-    });
-
-    // after that we have to increase the number then only next number will be increased.
-    setNum((previousState) => {
-      return(previousState + 1);
+  function updateCount(){
+    setCount( (previousState) => {
+      return (previousState + 1);
     });
   }
 
   return (
     <>
-      <h1> use array in state </h1>
-      <ul>
-        {list.map((list, index) => (
-          <li key={index}> {list} </li>
-        ))}
-      </ul>
-      {/* call addItem function */}
-      <button onClick={addItem}> add item </button>
+      <h1> I have rendered {count} times! </h1>
+      <button onClick={ updateCount }> increase count </button>
     </>
   );
 }
--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -9,9 +9,9 @@ import Color from './Componenet/Color'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <Color/>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
