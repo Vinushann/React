8d5d549571a5015d86514e586ba99e728a8e6dce
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-26 16:40:19 +0530

    True : A17 - Figure out the more lines of code in A16 section

--- learnreact/src/Componenet/form.js
+++ learnreact/src/Componenet/form.js
@@ -1,57 +1,55 @@
-// A16 - handle FORM  via object
 import { useState } from "react";
 
 function Myform() {
   // as OBJECT
   const [inputs, setInputs] = useState({});
-  
 
-  function submit(e){
+  function submit(e) {
     e.preventDefault();
     console.log(inputs);
   }
 
-
-  function storeAllData(e){
-
+  function storeAllData(e) {
     // get the name from the form
     const name = e.target.name;
 
-    // get the vale from that name
+    // get the value from that name
     const value = e.target.value;
 
-    setInputs( (previousState) => {
-      return {...previousState, [name] : value}
-    })
-    
+    setInputs((previousState) => {
+      // Update the state with the new input value
+      return { ...previousState, [name]: value };
+    });
+  }
 
   return (
     <>
-    <form action="" onSubmit={submit}>
-      
-      {/* get the name from form */}
-      <label htmlFor=""> Name : <br/>
-        <input type="text" name="name" onChange={storeAllData}/>
-      </label>
+      <form action="" onSubmit={submit}>
+        {/* get the name from form */}
+        <label htmlFor="">
+          Name : <br />
+          <input type="text" name="name" onChange={storeAllData} />
+        </label>
 
-      <br />
+        <br />
 
-        <label htmlFor=""> Age : <br/>
-        <input type="text" name="age" onChange={storeAllData}/>
-      </label>
+        <label htmlFor="">
+          Age : <br />
+          <input type="text" name="age" onChange={storeAllData} />
+        </label>
 
-      <br />
+        <br />
 
-        <label htmlFor=""> Address : <br/>
-        <input type="text"  name="address" onChange={storeAllData}/>
-      </label>
+        <label htmlFor="">
+          Address : <br />
+          <input type="text" name="address" onChange={storeAllData} />
+        </label>
 
-      <br /> <br />
-      <button>sumbit</button>
-    </form>
+        <br /> <br />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
-}
-export default Myform;
 
+export default Myform;
