b06f945a3937d878f6b342f98c5cb0746b68be0b
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-26 16:56:38 +0530

    A18 - set the intial value for input fields

--- learnreact/src/Componenet/form.js
+++ learnreact/src/Componenet/form.js
@@ -1,23 +1,28 @@
 import { useState } from "react";
 
 function Myform() {
-  // as OBJECT
-  const [inputs, setInputs] = useState({});
+ 
+  const [inputs, setInputs] = useState({
 
-  function submit(e) {
-    e.preventDefault();
-    console.log(inputs);
-  }
+    /* 
+        step 1 - set the intial value for that.
+    */
+    address : 'Colo'
+  });
+
+
+    function submit(e){
+
+      e.preventDefault();
+      console.log(inputs);
+    }
 
   function storeAllData(e) {
-    // get the name from the form
+    
     const name = e.target.name;
-
-    // get the value from that name
     const value = e.target.value;
 
     setInputs((previousState) => {
-      // Update the state with the new input value
       return { ...previousState, [name]: value };
     });
   }
@@ -25,7 +30,7 @@ function Myform() {
   return (
     <>
       <form action="" onSubmit={submit}>
-        {/* get the name from form */}
+       
         <label htmlFor="">
           Name : <br />
           <input type="text" name="name" onChange={storeAllData} />
@@ -42,11 +47,17 @@ function Myform() {
 
         <label htmlFor="">
           Address : <br />
-          <input type="text" name="address" onChange={storeAllData} />
+          {/* 
+              step 2 : use 'value' element for set the step - 1 value
+
+              step 3 : use full extension.
+                object-name . property-name
+          */}
+          <input type="text" name="address" value={inputs.address} onChange={storeAllData} />
         </label>
 
         <br /> <br />
-        <button type="submit">Submit</button>
+        <button type="submit"> Submit</button>
       </form>
     </>
   );
