82d6a2b1bc4bb5e6d0214e1355fceffd7c789eca
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-27 14:55:29 +0530

    A19 - Drop Down menu

--- learnreact/src/Componenet/form.js
+++ learnreact/src/Componenet/form.js
@@ -1,13 +1,21 @@
+/*
+  A19 - Drop Down menu
+*/
+
+/*
+  
+*/
+
 import { useState } from "react";
 
 function Myform() {
  
   const [inputs, setInputs] = useState({
 
-    /* 
-        step 1 - set the intial value for that.
+    /*
+      - set the default value for this object
     */
-    address : 'Colo'
+    country: 'India'
   });
 
 
@@ -32,28 +40,42 @@ function Myform() {
       <form action="" onSubmit={submit}>
        
         <label htmlFor="">
-          Name : <br />
+          Name: <br />
           <input type="text" name="name" onChange={storeAllData} />
         </label>
 
         <br />
 
         <label htmlFor="">
-          Age : <br />
+          Age: <br />
           <input type="text" name="age" onChange={storeAllData} />
         </label>
 
         <br />
 
         <label htmlFor="">
-          Address : <br />
-          {/* 
-              step 2 : use 'value' element for set the step - 1 value
-
-              step 3 : use full extension.
-                object-name . property-name
-          */}
+          Address: <br />
+          
           <input type="text" name="address" value={inputs.address} onChange={storeAllData} />
+        </label>
+
+          <br />
+
+        {/* 
+              step 1 : create a drop down menu
+        */}
+        <label htmlFor="">
+          country:
+        <br />
+          <select name="country" onChange={storeAllData} value={inputs.country}>
+            {/* first one for default */}
+            <option value="">Select</option>
+            <option value="srilanka"> Sri Lanka </option>
+            <option value="india"> India </option>
+            <option value="canada"> Canada </option>
+            <option value="america"> America </option>
+
+          </select>
         </label>
 
         <br /> <br />
@@ -64,3 +86,4 @@ function Myform() {
 }
 
 export default Myform;
+``
