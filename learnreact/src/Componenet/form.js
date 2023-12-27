d738778c6dbcd3a9b211382716260e3ad73e9ef5
Vinushann <vinushan.vimalraj@gmail.com>
2023-12-27 15:07:13 +0530

    A20 - Text Area

--- learnreact/src/Componenet/form.js
+++ learnreact/src/Componenet/form.js
@@ -1,22 +1,12 @@
 /*
-  A19 - Drop Down menu
-*/
-
-/*
-  
+  A20 - Text Area
 */
 
 import { useState } from "react";
 
 function Myform() {
  
-  const [inputs, setInputs] = useState({
-
-    /*
-      - set the default value for this object
-    */
-    country: 'India'
-  });
+  const [inputs, setInputs] = useState({});
 
 
     function submit(e){
@@ -39,44 +29,20 @@ function Myform() {
     <>
       <form action="" onSubmit={submit}>
        
+       {/* 
+          nothing is different from others all are same steps
+       */}
         <label htmlFor="">
-          Name: <br />
-          <input type="text" name="name" onChange={storeAllData} />
+          Tell just something: <br />
+
+          <textarea name="tell"
+                    value="you can type anything that you want !"
+                    onChange={storeAllData}>
+
+          </textarea>
         </label>
 
         <br />
-
-        <label htmlFor="">
-          Age: <br />
-          <input type="text" name="age" onChange={storeAllData} />
-        </label>
-
-        <br />
-
-        <label htmlFor="">
-          Address: <br />
-          
-          <input type="text" name="address" value={inputs.address} onChange={storeAllData} />
-        </label>
-
-          <br />
-
-        {/* 
-              step 1 : create a drop down menu
-        */}
-        <label htmlFor="">
-          country:
-        <br />
-          <select name="country" onChange={storeAllData} value={inputs.country}>
-            {/* first one for default */}
-            <option value="">Select</option>
-            <option value="srilanka"> Sri Lanka </option>
-            <option value="india"> India </option>
-            <option value="canada"> Canada </option>
-            <option value="america"> America </option>
-
-          </select>
-        </label>
 
         <br /> <br />
         <button type="submit"> Submit</button>
@@ -86,4 +52,3 @@ function Myform() {
 }
 
 export default Myform;
-``
